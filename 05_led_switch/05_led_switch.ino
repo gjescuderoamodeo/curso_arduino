27563e463b5d7a38c4b1b09d75b45697c54d84b7
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-15 12:10:53 +0200

    Update 05_led_switch.ino

--- 05_led_switch/05_led_switch.ino
+++ 05_led_switch/05_led_switch.ino
@@ -1,5 +1,6 @@
 #define BUTTON 13
 #define LED 4
+#define ZUM 15
 
 int buttonValue = LOW;
 int state = LOW;
@@ -7,15 +8,24 @@ int state = LOW;
 void setup() {
   pinMode(BUTTON, INPUT);
   pinMode(LED, OUTPUT);
+  pinMode(ZUM, OUTPUT);
 }
 
 void loop() {
   buttonValue = digitalRead(BUTTON);
   //if button press, change state
-  if(buttonValue == LOW){
+  /*if(buttonValue == LOW){
     state=!state;
     digitalWrite(LED, state);
   }  
 
+  delay(100);*/
+  if(buttonValue == LOW){
+    digitalWrite(LED, HIGH);
+    digitalWrite(ZUM, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+    digitalWrite(ZUM, LOW);
+  }
   delay(100);
 }
