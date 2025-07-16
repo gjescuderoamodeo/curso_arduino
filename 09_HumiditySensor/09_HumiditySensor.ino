cbea9e5438922c33b17061c6fc5d4924b69d9154
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-16 14:04:25 +0200

    adafruit
    library

--- 09_HumiditySensor/09_HumiditySensor.ino
+++ 09_HumiditySensor/09_HumiditySensor.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#define DHTPIN 12
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
+
+float temperature = 0.0;
+float humidity = 0.0;
+
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  delay(5000);
 
-}
+  Serial.printf("Temperatura: %.3f Humedad: %.3f \n", temperature, humidity);
+  }
