9404075960464069c0a228c4f4d26725d2fbc712
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-16 10:21:07 +0200

    Update 08_fade.ino

--- 05_led_switch/08_fade/08_fade.ino
+++ 05_led_switch/08_fade/08_fade.ino
@@ -1,8 +1,12 @@
 #define LED 15
+#define LED2 12
 
+int power = 1000;
+int power2 = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
 }
 
 void loop() {
@@ -16,22 +20,26 @@ void loop() {
   analogWrite(LED, 120);
   delay(500);*/
 
-  int power = 2000;
   do{
     analogWrite(LED, power);
+    analogWrite(LED2, power2);    
     delay(500); 
     power-=50;
-  }while(power!=0);
+    power2+=50;
+  }while(power!=0 && power2!=1000);
 
+  delay(1000);
+
+  do{
+    power+=50;
+    power2-=50;    
+    analogWrite(LED, power);
+    analogWrite(LED2, power2);
+    delay(500); 
+  }while(power!=1000 && power2!=0);
+
+  analogWrite(LED, 0);
+  analogWrite(LED2, 0);
   delay(500);
-  
-  if(power==0){
-    do{
-      analogWrite(LED, power);
-      delay(500); 
-      power+=50;
-    }while(power!=2000);
-  }
-
 
 }
