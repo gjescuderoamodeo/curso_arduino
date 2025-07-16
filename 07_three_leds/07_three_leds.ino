ea3ce9bc78e287cfa58bb1f59acde746b99ea352
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-16 12:43:54 +0200

    Update 07_three_leds.ino

--- 07_three_leds/07_three_leds.ino
+++ 07_three_leds/07_three_leds.ino
@@ -2,6 +2,9 @@
 #define LED_VERDE 12
 #define LED_AZUL 14
 
+#include <time.h>
+#include <stdlib.h>
+
 void setup() {
   pinMode(LED_ROJO, OUTPUT);
   pinMode(LED_VERDE, OUTPUT);
@@ -9,6 +12,21 @@ void setup() {
 }
 
 void loop() {
+  int r = 0;   
+  int g = 0; 
+  int b = 0; 
+
+  for(;;){
+    r = random(0,255);
+    g = random(0,255);
+    b = random(0,255);
+
+    analogWrite(LED_ROJO, r);
+    analogWrite(LED_VERDE, g);
+    analogWrite(LED_AZUL, b);
+    delay(1000);
+  }
+
   //ROJO
   digitalWrite(LED_ROJO, LOW);
   digitalWrite(LED_VERDE, HIGH);
@@ -28,10 +46,33 @@ void loop() {
   digitalWrite(LED_ROJO, HIGH);
   digitalWrite(LED_VERDE, HIGH);
   digitalWrite(LED_AZUL, LOW);
-  delay(200);
+  delay(1000);
+  //MAGENTA
+  digitalWrite(LED_ROJO, LOW);
+  digitalWrite(LED_VERDE, HIGH);
+  digitalWrite(LED_AZUL, LOW);
+  delay(1000);
+ //CIAN
+  digitalWrite(LED_ROJO, HIGH);
+  digitalWrite(LED_VERDE, LOW);
+  digitalWrite(LED_AZUL, LOW);
+  delay(1000);  
+  //BLANCO
+  digitalWrite(LED_ROJO, LOW);
+  digitalWrite(LED_VERDE, LOW);
+  digitalWrite(LED_AZUL, LOW);
+  delay(1000);
+
 
   digitalWrite(LED_ROJO, HIGH);
   digitalWrite(LED_VERDE, HIGH);
   digitalWrite(LED_AZUL, HIGH);
   delay(2000);  
+//----------------------------
+//ROJO
+  digitalWrite(LED_ROJO, LOW);
+  digitalWrite(LED_VERDE, HIGH);
+  digitalWrite(LED_AZUL, HIGH);
+  delay(1000);
+
 }
