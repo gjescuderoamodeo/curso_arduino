50f7b623656cef4f2b7528a563ee85fe919218a0
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-17 11:38:46 +0200

    Update 10_Arrays.ino

--- 10_Arrays/10_Arrays.ino
+++ 10_Arrays/10_Arrays.ino
@@ -1,10 +1,20 @@
 int numeros [5];
-int numeros2[] = (1,2,3,4,5,6,7);
+int numeros2[] = {1,2,3,4,5};
 
 void setup() {
-  numeros[0]=12;
+  Serial.begin(115200);
+  delay(100);
+  for(int i=0;i<5;i++){
+    numeros[i]=numeros2[i];
+  }
+
+  for(int i=0;i<5;i++){
+    Serial.println(numeros[i]);
+  }
+  
+
+
 }
 
 void loop() {
-
 }
