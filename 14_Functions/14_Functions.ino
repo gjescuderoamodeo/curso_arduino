0c9e492a131fe82a9b667ffb218a935e52beb775
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-18 10:25:04 +0200

    Update 14_Functions.ino

--- 14_Functions/14_Functions.ino
+++ 14_Functions/14_Functions.ino
@@ -1,8 +1,10 @@
 void setup() {
   Serial.begin(115200);
   int values [] = {1,2,3,4,5,8,12,33};
+  String cadena = "Arduino";
 
   Serial.println(String(sum(values)));
+  Serial.println(String(longitudCadena(cadena)));
 }
 
 void loop() {
@@ -11,11 +13,20 @@ void loop() {
 }
 
 //Functions
-  int sum(int values[]){
-    int sumTotal = 0;
-    for(int i=0;i<8;i++){
-      sumTotal += values[i]; 
-    }
+int sum(int values[]){
+  int sumTotal = 0;
+  for(int i=0;i<8;i++){
+    sumTotal += values[i]; 
+  }
 
-    return sumTotal;
+  return sumTotal;
+}
+
+//funcion calcule longitud de cadena
+int longitudCadena(String cadena){
+  for(int longitud=0;;longitud++){
+    if(cadena[longitud]==NULL){
+      return longitud;
+    }
+  }
 }
