c4796c155dc9332601b7aa07c468992e13f152a5
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-21 08:39:08 +0200

    Update 15_Wifi.ino

--- 14_Functions/15_Wifi/15_Wifi.ino
+++ 14_Functions/15_Wifi/15_Wifi.ino
@@ -11,7 +11,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\n mi Ip local:");
+  Serial.println("\nmi Ip local:");
   Serial.print(WiFi.localIP());
 }
 
@@ -21,6 +21,10 @@ void loop() {
 
   for(int i=0;i<redes;i++){
     Serial.print(WiFi.RSSI(i));
+    Serial.print("\t");
+    Serial.print(WiFi.channel(i));
+    Serial.print("\t");
+    Serial.println(WiFi.SSID(i).c_str()); //Devuelve nombre red convertido a cadena
   }
 
 }
