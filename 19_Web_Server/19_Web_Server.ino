c289515f2f7e78323baa7fd51c177ef9b6acb447
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-23 12:36:44 +0200

    Update 19_Web_Server.ino

--- 19_Web_Server/19_Web_Server.ino
+++ 19_Web_Server/19_Web_Server.ino
@@ -1,11 +1,18 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <DHT.h>
 
 #define PIN_LED 2
+#define DHTTYPE DHT22
 
 const String ssid = "Aula 1";
 const String pwd = "Horus.2025";
 WebServer server(80);
+
+//temperature
+DHT dht(DHTPIN, DHTTYPE);
+float temperature = 0.0;
+float humidity = 0.0;
 
 //páginas
 String home = "<!DOCTYPE html><html lang=\"es\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><title>Curso Arduino</title></head><style>body{background-color:#000;color:#adff2f;text-align:center;font-style:italic;font-family:Verdana,Geneva,Tahoma,sans-serif}table{display:inline}td{width:1%;height:50px;background-color:#ff4500}button{width:15%;background-color:#ff4500}a{color:#adff2f}</style><body><h1>Curso Arduino</h1><table><tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr><tr><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td></tr></table><button><a onclick=\"event.preventDefault()\" href=\"/led_change\">Encender</a></button><br><button><a onclick=\"event.preventDefault()\" href=\"/led_change\">Apagar</a></button></body></html>";
@@ -28,12 +35,15 @@ void setup() {
   server.on("/",handle_OnConnect);
   server.on("/led_change", setLed);
 
-
+  dht.begin();
   server.begin();
 }
 
 void loop() {
   server.handleClient();
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  delay(100);
 }
 
 //funciones de respuesta a las páginas web
