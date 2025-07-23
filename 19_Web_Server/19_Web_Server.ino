fabd3410b12668dc3ca3ea9fffb67fa29ba120a7
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-23 12:03:52 +0200

    Update 19_Web_Server.ino

--- 19_Web_Server/19_Web_Server.ino
+++ 19_Web_Server/19_Web_Server.ino
@@ -1,6 +1,8 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
+#define PIN_LED 2
+
 const String ssid = "Aula 1";
 const String pwd = "Horus.2025";
 WebServer server(80);
@@ -9,6 +11,8 @@ WebServer server(80);
 String home = "<!DOCTYPE html><html lang=\"es\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><title>Curso Arduino</title><style>body{background-color:#000;color:#adff2f;text-align:center;font-style:italic;font-family:Verdana,Geneva,Tahoma,sans-serif}table{display:inline}td{width:1%;height:50px;background-color:#ff4500}</style><body><h1>Curso Arduino</h1><table><tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr><tr><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td></tr></table></body></html>";
 void setup() {
   Serial.begin(115200);
+  pinMode(PIN_LED, OUTPUT);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid,pwd);
 
@@ -33,4 +37,14 @@ void loop() {
 //funciones de respuesta a las páginas web
 void handle_OnConnect(){
   server.send(200,"text/html",home);
+}
+
+void setLed(){
+  if(digitalRead(PIN_LED)!=LOW){
+    digitalWrite(PIN_LED, LOW);
+    delay(500); 
+  }else{
+    digitalWrite(PIN_LED, HIGH);
+    delay(500);
+  }
 }
