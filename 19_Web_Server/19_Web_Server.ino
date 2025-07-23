8c156bc31f098194741ff237a056d2f03e0c349f
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-23 10:23:09 +0200

    Update 19_Web_Server.ino

--- 19_Web_Server/19_Web_Server.ino
+++ 19_Web_Server/19_Web_Server.ino
@@ -5,6 +5,8 @@ const String ssid = "Aula 1";
 const String pwd = "Horus.2025";
 WebServer server(80);
 
+//páginas
+String home = "<!DOCTYPE html><html lang=\"es\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><title>Curso Arduino</title><style>body{background-color:#000;color:#adff2f;text-align:center;font-style:italic;font-family:Verdana,Geneva,Tahoma,sans-serif}table{display:inline}td{width:1%;height:50px;background-color:#ff4500}</style><body><h1>Curso Arduino</h1><table><tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr><tr><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td></tr></table></body></html>";
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -18,8 +20,17 @@ void setup() {
   Serial.println("\n mi Ip local:");
   Serial.print(WiFi.localIP());
 
+  //solicitudes web
+  server.on("/",handle_OnConnect);
+
   server.begin();
 }
 
 void loop() {
+  server.handleClient();
+}
+
+//funciones de respuesta a las páginas web
+void handle_OnConnect(){
+  server.send(200,"text/html",home);
 }
