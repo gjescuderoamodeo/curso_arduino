473cc0d0e72d68e340634cfb50825dcd8d5d5905
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-24 12:10:48 +0200

    Create 23_LCD_Temperature.ino

---
+++ 23_LCD_Temperature/23_LCD_Temperature.ino
@@ -0,0 +1,46 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <DHT.h>
+
+#define DHTPIN 18
+#define DHTTYPE DHT22
+
+#define SCL 15
+#define SDA 13
+#define ADDR 0x3F
+
+//temperature
+DHT dht(DHTPIN, DHTTYPE);
+float temperature = 0.0;
+float humidity = 0.0;
+
+//handler
+LiquidCrystal_I2C lcd(ADDR,16,2);
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(SDA,SCL);
+  dht.begin();
+
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
+
+void loop() {
+  
+  lcd.clear();
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  delay(100);
+
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura: " + String(temperature));
+  lcd.setCursor(0,1);
+  lcd.print("Humedad: " + String(humidity));
+  delay(5000);
+
+
+}  
+
