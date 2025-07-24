5521ac82bffdb342b854d0dabb3ace609e326189
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-24 09:53:04 +0200

    Update 20_LCD_Basic.ino

--- 20_LCD_Basic/20_LCD_Basic.ino
+++ 20_LCD_Basic/20_LCD_Basic.ino
@@ -1,15 +1,37 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-#define SCB 15
+#define SCL 15
 #define SDA 13
+#define ADDR 0x3F
+
+//handler
+LiquidCrystal_I2C lcd(ADDR,16,2);
 
 void setup() {
-  pinMode(SCB, OUTPUT);
-  pinMode(SDA, OUTPUT);
+  Wire.begin(SDA,SCL);
+  //Wire.beginTransmission(ADDR);
+
+  lcd.init();
+  //lcd.noBacklight();
+  //delay(1000);
+  lcd.backlight();
+  //delay(1000);
+  //lcd.blink();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Hola Mundo");
+  lcd.setCursor(0, 1);
+  lcd.print("Curso arduino");
+
+  //pinMode(SCL, OUTPUT);
+  //pinMode(SDA, OUTPUT);
 }
 
 void loop() {
-
+  lcd.display();
+  delay(500);
+  lcd.noDisplay();
+  delay(500);
 }  
 
