16aec636892c319352535e96fc7b44f5a1ad8867
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-24 13:49:43 +0200

    Create 24_Photoresistance.ino

---
+++ 24_Photoresistance/24_Photoresistance.ino
@@ -0,0 +1,39 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+#define SCL 15
+#define SDA 13
+#define ADDR 0x3F
+#define RESISTANCE 0
+
+int ledValue = 0;
+int resValue = 0;
+
+//handler
+LiquidCrystal_I2C lcd(ADDR,16,2);
+
+void setup() {
+  Wire.begin(SDA,SCL);
+  pinMode(RESISTANCE, INPUT);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+void loop() {
+  lcd.clear();
+  //0-4096
+  resValue=analogRead(RESISTANCE);
+
+  //valor convertido
+  ledValue = map(resValue,0,4096,0,255);
+  //analogWrite(LED, ledValue);
+
+
+
+  lcd.setCursor(0,0);
+  lcd.print("valor res:" + String(resValue));
+  delay(3000);
+}  
+
