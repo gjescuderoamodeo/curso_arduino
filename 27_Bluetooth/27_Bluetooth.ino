8474086d8858a5fab43b5ec1433b7b8b1f1a78ab
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-25 13:44:35 +0200

    Update 27_Bluetooth.ino

--- 27_Bluetooth/27_Bluetooth.ino
+++ 27_Bluetooth/27_Bluetooth.ino
@@ -1,6 +1,8 @@
 #include <ESP32Servo.h>
 #include <BluetoothSerial.h>
 #define PIN_SERVO 13
+#define PIN_RED 0
+#define PIN_GREEN 15
 
 //handler
 Servo miServo;
@@ -12,6 +14,8 @@ BluetoothSerial serialBT;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
   miServo.setPeriodHertz(50);
   miServo.attach(PIN_SERVO,500,2500);
   miServo.write(0);
@@ -28,8 +32,12 @@ void loop() {
     if(entrada==secreto){
       serialBT.println("Clave correcta");
       miServo.write(90);
+      analogWrite(PIN_GREEN, HIGHT);
+      analogWrite(PIN_RED, LOW);
       delay(5000);
       serialBT.println("Puerta cerrada");
+      analogWrite(PIN_GREEN, LOW);
+      analogWrite(PIN_RED, HIGHT);
       miServo.write(0);
     }else{
       serialBT.println("Clave NO CORRECTA");
