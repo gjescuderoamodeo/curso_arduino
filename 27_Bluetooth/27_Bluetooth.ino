62f599048620a7b46e699b8ac922b8e2a7e2890f
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-25 12:29:16 +0200

    Create 27_Bluetooth.ino

---
+++ 27_Bluetooth/27_Bluetooth.ino
@@ -0,0 +1,44 @@
+#include <ESP32Servo.h>
+#include <BluetoothSerial.h>
+#define PIN_SERVO 13
+
+//handler
+Servo miServo;
+
+String entrada="";
+String secreto="1234";
+
+BluetoothSerial serialBT;
+
+void setup() {
+  Serial.begin(115200);
+  miServo.setPeriodHertz(50);
+  miServo.attach(PIN_SERVO,500,2500);
+  miServo.write(0);
+  serialBT.begin("BT_Pepe");
+  Serial.println("Dispositivo listo para emparejarse");
+
+}
+
+void loop() {
+  //pedir introduzca código
+  if(Serial.available()){
+    serialBT.write(Serial.read());
+
+    entrada = Serial.readStringUntil('\n');
+    entrada.trim();
+    if(entrada==secreto){
+      Serial.println("Clave correcta");
+      miServo.write(90);
+      delay(5000);
+      Serial.println("Puerta cerrada");
+      miServo.write(0);
+    }else{
+      Serial.println("Clave NO CORRECTA");
+    }
+  }
+
+  if(serialBT.available()){
+    serial.write(serialBT.read());
+  }
+}
