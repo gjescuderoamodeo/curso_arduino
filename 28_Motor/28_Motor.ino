ea468969ee0b5c41dd6c2b1b069331ca035ea06b
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-28 11:28:39 +0200

    Update 28_Motor.ino

--- 28_Motor/28_Motor.ino
+++ 28_Motor/28_Motor.ino
@@ -13,23 +13,25 @@ void setup() {
 void loop() {
   //digitalWrite(VELOCITY, HIGH); //Encender motor
   Serial.println("Girando R");
-  analogWrite(VELOCITY, 90);
+  analogWrite(VELOCITY, 200);
   digitalWrite(DIRPIN_1, 0);
   digitalWrite(DIRPIN_2, 1);
   delay(2000);
 
   Serial.println("Paraó");
-  digitalWrite(VELOCITY, LOW); //Apagar motor
+  analogWrite(VELOCITY, 0);
+  //digitalWrite(VELOCITY, LOW); //Apagar motor
   delay(2000);
 
   Serial.println("Girando L");
   //digitalWrite(VELOCITY, HIGH); //Encender motor
-  analogWrite(VELOCITY, 100);
+  analogWrite(VELOCITY, 150);
   digitalWrite(DIRPIN_1, 1);
   digitalWrite(DIRPIN_2, 0); 
   delay(2000);
 
   Serial.println("Paraó");
-  digitalWrite(VELOCITY, LOW); //Apagar motor
+  analogWrite(VELOCITY, 0);
+  //digitalWrite(VELOCITY, LOW); //Apagar motor
   delay(2000); 
 }
