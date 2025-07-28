a25f5fe5bbf4b96d74028839af13191bbd0d5bc4
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-28 10:21:32 +0200

    Update 28_Motor.ino

--- 28_Motor/28_Motor.ino
+++ 28_Motor/28_Motor.ino
@@ -1,34 +1,35 @@
-#define INPUT_1_VELOCITY 15
-#define INPUT_2 13
-#define ENABLE_1 27
+#define VELOCITY 27
+#define DIRPIN_1 13
+#define DIRPIN_2 15
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(INPUT_1_VELOCITY, OUTPUT);
-  pinMode(INPUT_2, OUTPUT);
-  pinMode(ENABLE_1, OUTPUT);
+  pinMode(VELOCITY, OUTPUT);
+  pinMode(DIRPIN_1, OUTPUT);
+  pinMode(DIRPIN_2, OUTPUT);
 }
 
 void loop() {
-  //digitalWrite(INPUT_1_VELOCITY, HIGH); //Encender motor
+  //digitalWrite(VELOCITY, HIGH); //Encender motor
   Serial.println("Girando R");
-  analogWrite(INPUT_1_VELOCITY, 90);
-  digitalWrite(INPUT_2, 0);
-  digitalWrite(ENABLE_1, 1);
+  analogWrite(VELOCITY, 90);
+  digitalWrite(DIRPIN_1, 0);
+  digitalWrite(DIRPIN_2, 1);
   delay(2000);
 
   Serial.println("Paraó");
-  digitalWrite(INPUT_1_VELOCITY, LOW); //Apagar motor
+  digitalWrite(VELOCITY, LOW); //Apagar motor
   delay(2000);
 
   Serial.println("Girando L");
-  //digitalWrite(INPUT_1_VELOCITY, HIGH); //Encender motor
-  analogWrite(INPUT_1_VELOCITY, 100);
-  digitalWrite(INPUT_2, 1);
-  digitalWrite(ENABLE_1, 0); 
+  //digitalWrite(VELOCITY, HIGH); //Encender motor
+  analogWrite(VELOCITY, 100);
+  digitalWrite(DIRPIN_1, 1);
+  digitalWrite(DIRPIN_2, 0); 
   delay(2000);
-  
-  digitalWrite(INPUT_1_VELOCITY, LOW); //Apagar motor
+
+  Serial.println("Paraó");
+  digitalWrite(VELOCITY, LOW); //Apagar motor
   delay(2000); 
 }
