0498a6decfc1fbaf47c90780a6a9e03f0ed39a39
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-28 09:53:23 +0200

    Update 28_Motor.ino

--- 28_Motor/28_Motor.ino
+++ 28_Motor/28_Motor.ino
@@ -1,13 +1,22 @@
-#define INPUT_1 15
+#define INPUT_1_VELOCITY 15
 #define INPUT_2 13
 #define ENABLE_1 27
 
 void setup() {
-  
+  Serial.begin(115200);
 
+  pinMode(INPUT_1_VELOCITY, OUTPUT);
+  pinMode(INPUT_2, OUTPUT);
+  pinMode(ENABLE_1, OUTPUT);
 }
 
 void loop() {
-  
+  digitalWrite(INPUT_1_VELOCITY, HIGH); //Encender motor
+  digitalWrite(INPUT_2, 0);
+  digitalWrite(ENABLE_1, 1);
 
+  delay(5000);
+
+  digitalWrite(INPUT_1_VELOCITY, LOW); //Apagar motor
+  delay(2000);
 }
