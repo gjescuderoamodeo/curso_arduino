d41df89a4cef84dc8ff76ddd85b2ac46af501873
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-29 10:25:59 +0200

    Update 30_JoyStick.ino
    Add check if joystick goes up,down,right,left

--- 30_JoyStick/30_JoyStick.ino
+++ 30_JoyStick/30_JoyStick.ino
@@ -2,6 +2,9 @@
 #define VRY 0
 #define SW 13
 
+int cero_x=1892;
+int cero_y=2590;
+
 void setup() {
   Serial.begin(115200);
   pinMode(VRX, INPUT);
@@ -17,5 +20,22 @@ void loop() {
 
   Serial.println("Value X: " + String(value_x) + " Value Y: " + String(value_y) + " Value Z: " + String(value_z));
   
+  if(value_x-cero_x>10){ //DERECHA
+    Serial.println("DERECHA: " + String(value_x-cero_x));
+  }
+
+  if(value_x-cero_x<-10){ //IZQUIERDA
+    Serial.println("IZQUIERDA: " + String(value_x-cero_x));
+  }
+
+  if(value_y-cero_y>10){ //ABAJO
+    Serial.println("ABAJO: " + String(value_y-cero_y));
+  }
+
+  if(value_y-cero_y<-10){ //ARRIBA
+    Serial.println("ARRIBA: " + String(value_y-cero_y));
+  }
+
+
   delay(5000); 
 }
