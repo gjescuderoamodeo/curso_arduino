3c427b818890acd8d85f64bc9f3f900a9216e1e0
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-29 09:46:54 +0200

    Create 30_JoyStick.ino

---
+++ 30_JoyStick/30_JoyStick.ino
@@ -0,0 +1,21 @@
+#define VRX 4
+#define VRY 0
+#define SW 13
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(VRX, INPUT);
+  pinMode(VRY, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
+
+}
+
+void loop() {
+  int value_x=analogRead(VRX);
+  int value_y=analogRead(VRY);
+  int value_z=digitalRead(SW);
+
+  Serial.println("Value X: " + String(value_x) + "Value Y: " + String(value_y) + "Value Z: " + String(value_z));
+  
+  delay(5000); 
+}
