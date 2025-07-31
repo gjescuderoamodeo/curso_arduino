873ba050ac3a76e72fdaeebca70ffef41f717fa3
pepe <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-07-31 11:58:33 +0200

    Create 35_MQTT_temperature.ino

---
+++ 35_MQTT_temperature/35_MQTT_temperature.ino
@@ -0,0 +1,93 @@
+/*OTA:
+
+user:admin
+psswd:f@d64i"$XpkEq}+ | cursoArduino@1
+http://192.168.0.25:18083/
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <PubSubClient.h>
+
+//MQQT
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char* mqtt_server = "192.168.0.25";
+int mqtt_port = 1883;
+
+char* mqtt_topic_sub="curso_arduino/#";
+char* mqtt_topic_pub="curso_arduino/guillermo";
+char* mqtt_client_id=" ESP_PEPE";
+
+static unsigned long lastMsg = 0;
+
+//Wifi
+const String ssid = "Aula 1";
+const String pwd = "Horus.2025";
+
+WiFiClient clientWifi;
+HTTPClient http;
+
+String servidor = "http://192.168.0.56:5000/datos";
+
+
+void setup() {
+  Serial.begin(115200);
+  //conexión Wifi
+  WiFi.begin(ssid, pwd);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\n mi Ip local:");
+  Serial.print(WiFi.localIP());
+
+  //MQTT
+  client.setServer(mqtt_server,mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if(!client.connected()){
+    reconnect();
+  }
+  client.loop();
+
+  if (Serial.available() > 0) {
+    String mensaje = Serial.readStringUntil('\n');
+    mensaje.trim();  // Elimina espacios o saltos de línea
+    if (mensaje.length() > 0) {
+      client.publish(mqtt_topic_pub, mensaje.c_str());
+      Serial.print("Publicado: ");
+      Serial.println(mensaje);
+    }
+  }
+}
+
+//funciones
+void callback(String topic, byte* payload, unsigned int length){
+  Serial.print("Mensaje recibido en [");
+  Serial.print(topic);
+  Serial.print("]:");
+
+  for(int i=0; i<length; i++){
+    Serial.print((String)payload[i]);
+  }
+  Serial.print("");
+}
+
+void reconnect(){
+  while(!client.connected()){
+    Serial.println("Reconectando...");
+    if(client.connect(mqtt_client_id)){
+      Serial.println("Conectado a MQTT");
+      client.subscribe(mqtt_topic_sub);
+    }else{
+      Serial.println("Fallo en la conexión");
+      Serial.println("Error: " + client.state());
+      delay(5000);
+    }
+  }
+}
